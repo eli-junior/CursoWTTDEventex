cb42f5308b8cd059bd7e6279656994bb9c4436fd
Eli Júnior <elijr.net@gmail.com>
2021-03-09 17:00:35 -0300

    From alinhado com o sender identity

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -13,8 +13,8 @@ def subscribe(request):
             body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
             mail.send_mail('Confirmação de Inscrição',
                            body,
-                           'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+                           'realelijr@gmail.com',
+                           ['realelijr@gmail.com', form.cleaned_data['email']])
 
             messages.success(request, 'Inscrição Realizada com Sucesso!')
 
